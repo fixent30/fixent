343bb2c425a1da16153a7cb7f9478105c9d0ad3a
Arindam242 <creativeab26@gmail.com>
2022-02-15 15:22:41 +0530

    import the Product section

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 import Services from '../components/Services'
+import Products from '../components/Products'
 import { db } from '../utils/firebase'
 import getCollectionData from '../utils/getCollectionData'
 
