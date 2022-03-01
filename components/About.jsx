f5be165de947e964e88e26a5b1c857f582922b37
Arindam242 <creativeab26@gmail.com>
2022-03-01 18:24:19 +0530

    fiuxed the map

--- components/About.jsx
+++ components/About.jsx
@@ -4,13 +4,13 @@ const About = () => {
   return (
     <div
       name="About"
-      className="flex flex-col lg:w-[80%]  mx-auto lg:flex-row space-x-4 items-center my-4 space-y-5"
+      className="flex flex-col lg:w-[80%] w-[95%] mx-auto lg:flex-row space-x-4 items-center my-4 space-y-5"
     >
       <img
         src="/logo.png"
         className="border-4 w-36 h-36 lg:w-40 lg:h-40 border-[#ee2b55] rounded-full cursor-pointer"
       />
-      <div className="bg-[#ee2b55] lg:text-xl p-4 lg:p-6 rounded-md text-white font-bold">
+      <div className="bg-[#ee2b55] w-full lg:text-xl p-4 lg:p-6 rounded-md text-white font-bold">
         100% BEST PRICE WE DEAL IN:- Laptop Battery | Laptop keyboard | Laptop
         Harddisk | Laptop Ram | Desktop Ram | Laptop Adapters | Refurbished
         Adapter| Laptop Cuddy | Laptop Screen | Laptop Casing| Wifi Adapter|
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <div className="w-full flex flex-col md:flex-row items-center justify-between">
         <img src="/logo.png" className="h-32  w-32 lg:w-64 lg:h-64" />
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3569.769106514028!2d88.7272307154413!3d26.527549382950003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e4794097acc1c3%3A0xb08f61024e7d83cc!2sSaptarshi%20Apartment%2C%20Jalpaiguri%2C%20West%20Bengal%20735101!5e0!3m2!1sen!2sin!4v1644064692101!5m2!1sen!2sin"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3569.9045261683013!2d88.71209501503674!3d26.523194883296924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe258fad057cb031b!2zMjbCsDMxJzIzLjUiTiA4OMKwNDInNTEuNCJF!5e0!3m2!1sen!2sin!4v1646139202384!5m2!1sen!2sin"
           width="400"
           height="300"
           className="border-0 rounded-sm m-4"
@@ -12,8 +12,8 @@ const Footer = () => {
         ></iframe>
       </div>
       <h2 className="my-2 text-white font-bold text-lg">
-      fixentrepairjalpaiguri@gmail.com
-    </h2>
+        fixentrepairjalpaiguri@gmail.com
+      </h2>
     </footer>
   );
 };
